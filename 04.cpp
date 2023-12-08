4bd35119911f15b6183eb720189a9db7e05760f3
Mark <markbravo0312@gmail.com>
2023-12-08 17:33:44 +0100

    finished day 4

--- 04.cpp
+++ 04.cpp
@@ -2,7 +2,7 @@
 #include "Parser.h"
 #include <unordered_set>
 
-
+#define NUMCARDS 208
 void part1(const std::vector<std::string> & lines) {
     int points{0};
     for (auto & line : lines) {
@@ -36,9 +36,48 @@ void part1(const std::vector<std::string> & lines) {
     std::cout << "Result part1:" << points << std::endl;
     
 }
+// 1: 3 matches, 1 card
+// 2: 2 matches, 1 card                 ->  2 matches, 2 cards
+// 3: 1 match, 1 card                   ->  1 match, 2 cards -> 1 match, 3 cards
+// 4: 0 matches                     
+void part2(const std::vector<std::string> & lines) {
+    int points{0};
+    std::array<int, NUMCARDS> counter{};
+    counter.fill(1);
+    int cards{NUMCARDS};
+    for (int i = 0; i < lines.size(); ++i) {
+        
+        bool mycards = false;
+        std::vector<std::string> splitted = split(lines[i], ' ');
+        std::unordered_set<std::string> set{};
+        int matches {0};
+        for (auto a : splitted) {
+            if (a == "|") {
+                mycards = true;
+            }
 
-void part2() {
-    
+            if (!mycards) {
+                set.insert(a);
+            } else {
+                if (set.find(a) != set.end()) {
+                    matches++;
+                }
+            }
+        } 
+
+        
+        int summand = counter[i] * matches;
+       
+
+        for (int k = 0;  k < matches && ((i + 1 + k) < counter.size()); ++k) {
+            counter[i + 1 + k] += counter[i];
+        }
+
+        cards += summand;
+        mycards =  false;
+    }
+
+    std::cout << "Result part2:" << cards << std::endl;
 }
 
 
@@ -46,6 +85,7 @@ int main()  {
 
     auto lines = Parser::readLines("../input.txt");
     part1(lines);
+    part2(lines);
 
 
 
---
+++ 05.cpp
@@ -0,0 +1,19 @@
+#include "Parser.h"
+
+
+
+void part1 () {
+
+}
+
+
+
+void part2 () {
+    
+}
+
+
+int main () {
+
+    auto lines = 
+}
