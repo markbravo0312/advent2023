417c18b93944421434e32f0c3bca095d3a940e9d
Mark <markbravo0312@gmail.com>
2023-12-09 11:28:40 +0100

    update push

--- 05.cpp
+++ 05.cpp
@@ -1,19 +1,36 @@
 #include "Parser.h"
+#include <set>
 
 
+std::set<int> seeds{};
 
-void part1 () {
 
+void parseSeeds(const std::string& line) {
+    std::vector<std::string> splitted = split(line, ' ');
+    for (int i = 1; i < splitted.size(); ++ i) {
+        seeds.insert( std::stoi(splitted[i]) );
+    }
+}
+
+void part1 (const std::vector<std::string>& lines) {
+
+    parseSeeds(lines[0]);
+    
+    
 }
 
 
 
-void part2 () {
+void part2 (const std::vector<std::string>& lines) {
     
 }
 
 
 int main () {
 
-    auto lines = 
+    auto lines = Parser::readLines("../input.txt");
+    
+    part1(lines);
+    part2(lines);
+
 }
